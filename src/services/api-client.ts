78ad16bf270bc84a90a9fa73ce0ac4b0df1d3630
Robert <robert.jones@cromwell.co.uk>
2023-05-25 12:08:18 +0100

    fetch game and env set up

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'https://api.rawg.io/api',
+    baseURL: "https://api.rawg.io/api",
     params: {
-        key: 'd1d5ef4dd64e4f95a79a7eae193622a3'
+        key: import.meta.env.VITE_API_KEY
     }
 })
